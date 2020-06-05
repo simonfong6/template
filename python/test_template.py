7ccce1df7efda2f6d66c24cbdaae2836e44fdce0
Simon <simonfong6@gmail.com>
2020-06-05 02:47:55 -0700

    python test template

---
+++ python/test_template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Test Template
+
+Template for a test.
+"""
+
+import unittest
+
+
+class TestTemplate(unittest.TestCase):
+
+    def test_template(self):
+        """Template test.
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
